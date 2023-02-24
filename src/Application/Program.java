eb53c108d2c79b13ffa757705bb4fec14331e74e
kenedy gomes <kndgfls5@gmail.com>
2023-02-23 21:48:07 -0300

    Create class Departament and seller

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,10 @@
 package Application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +12,11 @@ public class Program {
 	    
 		Departament obj = new Departament(1 , "books");
 				System.out.println(obj);
+		
+		SimpleDateFormat sdt = new SimpleDateFormat("dd/MM/yyyy");		
+				
+		Seller sel = new Seller(1, "kenedy", "kndgfls5@gmail.com", new Date(), 3000.0, obj);		
+		System.out.println(sel);
 	}
 
 }
